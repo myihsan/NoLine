ba1d51c913d6b4b5e0436c01a0210b67e4ea51c7
Ihsan <ihsan.lee@gmail.com>
2015-05-04 17:50:51 +0800

    Add the navigation drawer

--- app/src/main/java/org/ihsan/android/noline/QueueListActivity.java
+++ app/src/main/java/org/ihsan/android/noline/QueueListActivity.java
@@ -1,10 +1,21 @@
 package org.ihsan.android.noline;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.tencent.android.tpush.XGIOperateCallback;
 import com.tencent.android.tpush.XGPushConfig;
 import com.tencent.android.tpush.XGPushManager;
@@ -38,5 +49,45 @@ public class QueueListActivity extends SingleFragmentActivity {
                 Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
             }
         });
+
+        AccountHeader.Result headerResult = new AccountHeader()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.drawer_header)
+                .withHeaderBackgroundScaleType(ImageView.ScaleType.CENTER_CROP)
+                .build();
+
+        Drawer.Result result = new Drawer()
+                .withActivity(this)
+                .withToolbar(mToolbar)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("我要排队").withIcon(R.drawable
+                                .ic_format_list_numbered_grey600_24dp),
+                        new PrimaryDrawerItem().withName("排队记录").withIcon(R.drawable
+                                .ic_history_grey600_24dp)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long
+                            id, IDrawerItem drawerItem) {
+                        FragmentManager fragmentManager = getFragmentManager();
+                        Fragment fragment;
+                        switch (position) {
+                            case 0:
+                                fragment = new QueueListFragment();
+                                fragmentManager.beginTransaction()
+                                        .replace(R.id.fragmentContainer, fragment)
+                                        .commit();
+                                break;
+                            case 1:
+                                fragment = new Fragment();
+                                fragmentManager.beginTransaction()
+                                        .replace(R.id.fragmentContainer, fragment)
+                                        .commit();
+                                break;
+                        }
+                    }
+                })
+                .build();
     }
 }
